45e34191f3e7d94491f6b002060288704d5e9110
LeonardoChase <32912065+LeonardoChase@users.noreply.github.com>
2022-02-01 09:49:11 -0600

    Add files via upload

--- src/Heart.java
+++ src/Heart.java
@@ -2,30 +2,73 @@ public class Heart {
     // PROPERTIES
     private String color;
     //Add remaining heart properties here...
-
+    private int size;
+    private String sayings;
+    private int calories;
+    private double cost;
+    private boolean avalible;
+    private int stock;
 
     // CONSTRUCTORS
-    public Heart() {} // This is the default contructor
+    public Heart() {
+        this.color = "";
+        this.size = 0;
+        this.sayings = "";
+        this.calories = 0;
+        this.cost = 0.00;
+        this.stock = 0;
+    } // This is the default contructor
     //Add overloaded Constructor here...
-
+    public Heart(String color, int size, String sayings, int calories, double cost, int stock) {
+        this.color = color;
+        this.size = size;
+        this.sayings = sayings;
+        this.calories = calories;
+        this.cost = cost;
+        this.stock = stock;
+        if(this.stock>0) {
+            this.avalible = true;
+        }
+    }
 
     // ACCESSORS
     public String getColor() {
         return this.color;
     }
     //Add remaining Accessor methods here...
+    public int getSize() {
+        return this.size;
+    }
+    public String getSayings() {
+        return this.sayings;
+    }
+    public int getCalories() {
+        return this.calories;
+    }
+    public double getCost() {
+        return this.cost;
+    }
+    public boolean getAvalible() {
+        return this.avalible;
+    }
+    public int getStock() {
+        return this.stock;
+    }
+    public String getReport() {
+        String report = "Color: "+ this.color + "\n" + "Size: "+ this.size + "\n" + "Saying: "+ this.sayings + "\n" + "Calories: "+ this.calories + "\n" + "Cost: "+ this.cost + "\n" + 
+        "Avalible: "+ this.avalible + "\n" + "Stock: "+ this.stock + "\n";
+        return report;
+    }
 
 
     // MUTATORS
-    public void setColor(String color) {
-        this.color = color;
+    public void adjCost(double percentage) {
+        this.cost = this.cost + (this.cost*percentage/100);
     }
-    //Add remaining Mutator methods here...
-
-
-    // METHODS
-    public String report() {
-        return this.color + "\n"; // Add other properties here as you add them to the class
+    public void inStock(int stock) {
+        this.stock = stock;
+        if (this.stock>0) {
+            this.avalible = true;
+        }
     }
-    //Write other methods here (if needed)...
 }
--- src/HeartsClient.java
+++ src/HeartsClient.java
@@ -1,5 +1,28 @@
 public class HeartsClient {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args){
+
+      Heart group1 = new Heart("red",3,"Hi",0,5.00,50);
+      Heart group2 = new Heart("teal",3,"Love CS-A",0,4.50,200);
+      Heart group3 = new Heart("green",2,"Go Girl!",0,3.50,75);
+
+      System.out.println(group1.getReport());
+      System.out.println(group2.getReport());
+      System.out.println(group3.getReport());
+
+      Heart [] store = new Heart[3];
+      store[0]=group1;
+      store[1]=group2;
+      store[2]=group3;
+
+      System.out.println(buyOut (store));
+    }
+
+    public static int buyOut (Heart[] store) {
+        int total=0;
+        for (int i=0; i<store.length; i++) {
+            total = total + store[i].getStock();
+        }
+
+        return total;
     }
 }
